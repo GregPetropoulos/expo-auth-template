d937f8785f4462f4ef7233244ed7cb2b94df1335
GP <gregpetropoulos@yahoo.com>
2024-05-06 01:01:21 -0400

    Basic button test for calling the onPress function

--- components/__tests__/Button.test.tsx
+++ components/__tests__/Button.test.tsx
@@ -1,8 +1,22 @@
-import Button from "../Button";
+import { fireEvent, render, screen } from 'expo-router/testing-library';
+
+import Button from '../Button';
 // it('test',()=>{
 //   console.log('tested button')
 // })
 
-it('button', () => {
-  expect(1).toBe(1);
+// it('button', () => {
+//   expect(1).toBe(1);
+// });
+describe(`Custom Button`, () => {
+  it('Calls onPress function when the button is pressed', () => {
+    const mockPress = jest.fn();
+    const { getByTestId } = render(<Button onPress={mockPress}>Sign In</Button>);
+    const pressMeButton = getByTestId('CustomButton');
+    fireEvent.press(pressMeButton);
+    expect(mockPress).toHaveBeenCalled();
+  });
+
+  // const tree = render(<Button value={'Test'} onPress={mockPress}>SignIn</Button>);
+  // expect(tree).toMatchSnapshot();
 });
